3b4564ead09751210abc5bdbfe823726be21e657
JungDongSeop <ds123911@gmail.com>
2025-07-08 15:16:23 +0900

    refactor: 주석, 로그 정리

--- src/main/java/com/pieceofcake/alert_service/alert/application/AlertService.java
+++ src/main/java/com/pieceofcake/alert_service/alert/application/AlertService.java
@@ -2,7 +2,6 @@ package com.pieceofcake.alert_service.alert.application;
 
 import com.pieceofcake.alert_service.alert.dto.out.AlertResponseDto;
 import com.pieceofcake.alert_service.alert.entity.enums.AlertType;
-import com.pieceofcake.alert_service.alert.vo.out.AlertResponseVo;
 import com.pieceofcake.alert_service.kafka.event.AlertKafkaEvent;
 import reactor.core.publisher.Flux;
 
--- src/main/java/com/pieceofcake/alert_service/alert/presentation/AlertController.java
+++ src/main/java/com/pieceofcake/alert_service/alert/presentation/AlertController.java
@@ -2,12 +2,8 @@ package com.pieceofcake.alert_service.alert.presentation;
 
 import com.pieceofcake.alert_service.alert.application.AlertService;
 import com.pieceofcake.alert_service.alert.dto.out.AlertResponseDto;
-import com.pieceofcake.alert_service.alert.vo.out.AlertResponseVo;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
